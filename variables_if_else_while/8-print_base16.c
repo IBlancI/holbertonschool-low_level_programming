7cc2e9d01efc96024f4a0696bc05b681c13c0c84
root <IBlancI>
2023-10-16 12:09:28 -0700

    Task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,10 +11,10 @@ int main(void)
 {
 	char letter;
 	int num;
-	
+
 	for (num = 0; num <= 9; num++)
 		putchar('0' + num);
-	
+
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
 	putchar('\n');
