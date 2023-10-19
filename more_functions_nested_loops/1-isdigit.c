b6604d9d076a89d09bedc0fe82590481ba7e38f3
root <IBlancI>
2023-10-19 14:10:40 -0700

    Task 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -2,15 +2,17 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code
- * @c: value to check
- * Description: fvgbsdf
- * Return: 1 if uppercase else 0
- */
+ * _isdigit - check if c is digit
+ *
+ * @c: The ASCII value of the paramater to be checked.
+ *
+ * Description: check if it's a number or not
+ * Return: 1 if it's a digit or 0 if not
+*/
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= '0' && c <= '9')
 
 	return (1);
 	return (0);
