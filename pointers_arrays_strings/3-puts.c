df4b13fd4f0103d6afa75769aca35e8cd1bd0235
root <IBlancI>
2023-10-24 01:57:41 -0700

    Task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,14 +2,15 @@
 #include "main.h"
 
 /**
-  * puts - Entry point
-  * @stri
+  * _puts - Entry point
+  * @str: variable
   * Return: always 0 (success)
   */
 
 void _puts(char *str)
 {
 	int lettre;
+
 	for (lettre = 0; str[lettre]; lettre++)
 	{
 		putchar(str[lettre]);
